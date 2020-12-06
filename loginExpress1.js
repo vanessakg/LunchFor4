e86a2bea3e08d2f425e9035cd1fc2fdd3e783bed
JakeBucher <armyjake101@gmail.com>
2020-12-06 15:26:07 -0500

    updaated 12/6
    updated 12/6

--- loginExpress1.js
+++ loginExpress1.js
@@ -15,22 +15,17 @@ var con = mysql.createConnection({
    database: "lunch44g4"
 });
 
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
 app.set('view engine', 'pug' );
 app.use( express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
-}));
+
 app.get('/LFFstart', function(req, res){
     res.render('loginTest1')
-});
-app.get('/adminLogin', function(req, res){
-    res.render('adminLogin')
-});
-app.get('/admin', function(req, res){
-    res.render('admin')
 });
 app.get('/LFFcreate', function(req, res){
     res.render('createGroup')
@@ -44,10 +39,17 @@ app.get('/LFFpostLogin', function(req, res){
 app.get('/LFFcreateMember', function(req, res){
     res.render('accountCreate')
 });
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
+app.get('/adminLogin', function(req, res){
+    res.render('adminLogin')
 });
+app.get('/admin', function(req, res){
+    res.render('admin')
+});
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
 app.get('/', function(request, response) {
 	response.sendFile(path.join(__dirname + '/adminLogin'));
 });
@@ -81,20 +83,28 @@ app.get('/admin', function(request, response) {
 app.post('/groupCreated', function(req, res){
     console.log(req.body);
 
-    var sql = "INSERT INTO GroupInfo(time, location, groupCode, date) VALUES ('"+req.body.time+"','"+req.body.location+"','"+req.body.groupCode+"','"+req.body.gDate+"')";
+    var sql = "INSERT INTO GroupInfo(time, location, groupCode, date)  \
+               VALUES ('"+req.body.time+"','"+req.body.location+"','"+req.body.groupCode+"','"+req.body.gDate+"')";
     con.query(sql, function(err){
         if(err) throw err;
-        console.log("record inserted!"); 
+        console.log("record inserted!");  
     });
     res.send("Your information has been submitted: " + "\nTIME: " + req.body.time + "GROUP CODE: " +
     req.body.groupCode + "DATE: " + req.body.gDate); 
 
+    const message = {
+        from: 'lunch4four@aurora.edu', 
+        to: '0772a8d93c-cc7bce@inbox.mailtrap.io',
+        subject: 'Information about your Lunch4Four Monthly Meeting',
+        text: `Here is your group information: Group Code:${req.body.groupCode}, Time:${req.body.time}, Location:${req.body.location},  Date:${req.body.gDate}` 
+    };
+    
     transporter.sendMail(message, function(err, info){
         if(err){
             console.log(err)
-        } else {
+        }else {
             console.log(info)
-            var sql2 = "SELECT * FROM Member order by rand() LIMIT 2";
+            var sql2 = "SELECT * FROM Member order by rand() LIMIT 4";
             con.query(sql2, function(err, rows){
                 if(err){
                     console.log(err)
@@ -108,33 +118,50 @@ app.post('/groupCreated', function(req, res){
     });
 });
 
-// app.post('/loginLFF', function(req, res){
-//     console.log(body);
-//     var sql = "INSERT INTO Member(fName, lName, email, phoneNum, au_id) VALUES ('"+req.body.fName+"', '"+req.body.lName+"', '"+req.body.email+"', '"+req.body.phoneNum+"', '"+req.body.au_id+"')";
-//     con.query(sql, function(err){
-//         if(err) throw err;
-//         console.log("Member updated");
-//     })
-// })
+app.post('/memberCreated', function(req, res){
+    console.log(req.body);
+    var sql = "INSERT INTO Member(fName, lName, email, phoneNum, au_id, Affiliation, acc_Activity) VALUES  \
+               ('"+req.body.fName+"', '"+req.body.lName+"', '"+req.body.email+"', '"+req.body.phoneNum+"', \
+                '"+req.body.au_id+"', '"+req.body.Affiliation+"', '"+req.body.acc_Activity+"')";
+    con.query(sql, function(err){
+        if(err) throw err;
+        console.log("Member updated");
+    })
 
-let transporter = nodemailer.createTransport({
-    host: 'smtp.mailtrap.io',
-    port: 2525,
-    auth: {
-        user: '4ec3e368bdf5e0',
-        pass: 'ca40c475b2f8b0'
-    }
+    res.render('postLogin');
 });
 
+var transporter = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "65a2c5760d7b03",
+      pass: "16a9c595af29ae"
+    }
+  });
+
+app.post('/memberCreated', function(req, res){
+    console.log(req.body);
+    var sql = "UPDATE TABLE Member SET acc_Activity = 'Active' WHERE acc_Activity = 'Inactive'";
+    con.query(sql, function(err, result){
+        if(err) throw err;
+        console.log(result.affectedRows + " record(s) updated");
+    });
+});
+
+//Delete member if they want to be inactive..? 
+app.delete('/memberCreated', function(req, res){
+    var sql = "DELETE Member WHERE acc_Activity = 'Inactive'";
+    con.query(sql, function(err, rows){
+        if(err) throw err;
+        console.log("Deleted successfully: ", rows);
+        res.send('Deleted successfully');
+    });
+});
 //login to mailtrap.io
 // email: vanessagill94@hotmail.com
 // pw: lunch4four!
-const message = {
-    from: 'lunch4four@aurora.edu', 
-    to: '0772a8d93c-c06d1a@inbox.mailtrap.io',
-    subject: 'Information about your Lunch4Four Monthly Meeting',
-    text: 'Thanks for signing up for Lunch4Four!' // Send out groupInfo
-};
+
 //This syntax sends out email every 4th of month
 // cron.schedule('0 0 4 * *', () => {
 //     if(error){
@@ -146,4 +173,4 @@ const message = {
 //app.post('/joinGroup', function(req, res){    
 //});
 module.exports = con;
-app.listen(3333); 
+app.listen(3333);   
